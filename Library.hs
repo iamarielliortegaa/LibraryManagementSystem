d1e6f5630c0067a5e3acf523aa435f24b45bbba8
iamarielliortegaa <arielli.ortegaa@gmail.com>
2025-11-16 14:54:52 +0530

    Introduced books and users related commands.

--- Library.hs
+++ Library.hs
@@ -6,84 +6,185 @@ data Status
   deriving (Eq, Show)
 
 data Book = Book
-  { bookId :: Int,
-    title :: String,
-    author :: String,
-    status :: Status
+  { bookId       :: Int,
+    title        :: String,
+    author       :: String,
+    status       :: Status,
+    borrowerName :: Maybe String
   }
   deriving (Show, Eq)
 
 data User = User
-  { userId :: Int,
+  { userId   :: Int,
     userName :: String
   }
   deriving (Show, Eq)
 
 data Library = Library
-  { books :: [Book],
-    users :: [User],
+  { books     :: [Book],
+    users     :: [User],
     bookCount :: Int,
-    userCount :: Int,
-    borrowerName :: Maybe String
+    userCount :: Int
   }
   deriving (Show, Eq)
 
 library :: Library
 library =
   Library
-    { books = [],
-      users = [],
+    { books     = [],
+      users     = [],
       bookCount = 0,
       userCount = 0
     }
 
--- methods
+-- Book Section
 
 -- Add a new Book
 addBook :: String -> String -> Library -> Library
 addBook inputTitle inputAuthor inputLibrary =
-  let netBookCount = bookCount inputLibrary + 1
+  let newBookCount = bookCount inputLibrary + 1
 
       newBook =
         Book
-          { bookId = netBookCount,
-            title = inputTitle,
-            author = inputAuthor,
-            status = Available
+          { bookId       = newBookCount,
+            title        = inputTitle,
+            author       = inputAuthor,
+            status       = Available,
+            borrowerName = Nothing
           }
    in inputLibrary
-        { books = books inputLibrary ++ [newBook],
-          bookCount = netBookCount
+        { books     = books inputLibrary ++ [newBook],
+          bookCount = newBookCount
         }
+
+-- Remove a Book
+removeBook :: Int -> Library -> (Library, String)
+removeBook inputBookId inputLibrary =
+  let allBooks    = books inputLibrary
+      targetBooks = [book | book <- allBooks, bookId book == inputBookId]
+      otherBooks  = [book | book <- allBooks, bookId book /= inputBookId]
+  in
+    case targetBooks of
+      [] -> (inputLibrary, "Book ID is invalid!")
+  
+      [book] ->
+        case status book of
+          Borrowed -> (inputLibrary, "Book is borrowed!")
+          Available -> (inputLibrary {books = otherBooks}, "Book removed!")
+
+
+-- Borrow Book
+borrowBook :: Int -> Int -> Library -> (Library, String)
+borrowBook inputBookId inputUserId inputLibrary =
+  let allBooks = books inputLibrary
+      allUsers = users inputLibrary
+      targetBooks = [book | book <- allBooks, bookId book == inputBookId]
+      targetUsers  = [user | user <- allUsers, userId user == inputUserId]
+  in
+    case targetBooks of
+      [] -> (inputLibrary, "Book ID is invalid!")
+
+      [book] ->
+        case status book of
+          Borrowed -> (inputLibrary, "Book is borrowed!")
+          Available -> (inputLibrary, "Book is available!")    -- TODO
+
+-- Return Book
+returnBook :: Int -> Int -> Library -> (Library, String)
+returnBook inputBookId inputUserId inputLibrary =
+  let allBooks = books inputLibrary
+      allUsers = users inputLibrary
+      targetBooks = [book | book <- allBooks, bookId book == inputBookId]
+      targetUsers  = [user | user <- allUsers, userId user == inputUserId]
+  in
+    case targetBooks of
+      [] -> (inputLibrary, "Book ID is invalid!")
+
+      [book] ->
+        case status book of
+          Available -> (inputLibrary, "Book is available!")
+          Borrowed -> (inputLibrary, "Book is borrowed!")  -- TODO
+          
 
 -- Print Available Books
 availableBooks :: [Book] -> IO ()
-availableBooks [] = putStrLn ""
+availableBooks [] = putStrLn "No available books."
 availableBooks allBooks = do
   let availableBookList = [book | book <- allBooks, status book == Available]
+  putStrLn "Available Books:"
   printBooks availableBookList
 
--- Print Available Books
+-- Print Borrowed Books
 borrowedBooks :: [Book] -> IO ()
-borrowedBooks [] = putStrLn ""
+borrowedBooks [] = putStrLn "No borrowed books."
 borrowedBooks allBooks = do
   let borrowedBookList = [book | book <- allBooks, status book == Borrowed]
+  putStrLn "Borrowed Books:"
   printBooks borrowedBookList
 
 -- Print Books
 printBooks :: [Book] -> IO ()
-printBooks [] = putStrLn ""
-printBooks (book : books) = do
+printBooks [] = return ()
+printBooks (book : otherBooks) = do
   putStrLn "-----------------------------------------"
   putStrLn ("Book Id     : " ++ show (bookId book))
-  putStrLn ("Book Name   : " ++ show (title book))
-  putStrLn ("Book Author : " ++ show (author book))
+  putStrLn ("Book Name   : " ++ title book)
+  putStrLn ("Book Author : " ++ author book)
   putStrLn ("Book Status : " ++ show (status book))
 
-  case borrower book of
-    Nothing -> putStrLn ""
-    Just borrowerName ->
-      putStrLn ("Book Borrower: " ++ show (borrowerName))
+  case borrowerName book of
+    Nothing ->
+      return ()
+    Just name ->
+      putStrLn ("Book Borrower: " ++ name)
 
   putStrLn "-----------------------------------------"
-  printBooks books
+  printBooks otherBooks
+
+-- User Section
+
+-- Add a new User
+addUser :: String -> Library -> Library
+addUser inputName inputLibrary =
+  let newUserCount = userCount inputLibrary + 1
+
+      newUser =
+        User
+          { userId       = newUserCount,
+            userName     = inputName
+          }
+   in inputLibrary
+        { users     = users inputLibrary ++ [newUser],
+          userCount = newUserCount
+        }
+
+-- Print Users
+printUsers :: [User] -> IO ()
+printUsers [] = return ()
+printUsers (user : otherUsers) = do
+  putStrLn "-----------------------------------------"
+  putStrLn ("User Id     : " ++ show (userId user))
+  putStrLn ("User Name   : " ++ userName user)
+  putStrLn "-----------------------------------------"
+  printUsers otherUsers
+
+
+-- Remove a User
+removeUser :: Int -> Library -> (Library, String)
+removeUser inputUserId inputLibrary =
+  let allUsers    = users inputLibrary
+      allBooks    = books inputLibrary
+      
+      targetUsers  = [user | user <- allUsers, userId user == inputUserId]
+      otherUsers  = [user | user <- allUsers, userId user /= inputUserId]
+  in
+    case targetUsers of
+      [] -> (inputLibrary, "User ID is invalid!")
+
+      [user] ->
+        let name = userName user
+            targetBooks = [book | book <- allBooks, borrowerName book == Just name]
+        in
+          case targetBooks of
+            [] -> (inputLibrary {users = otherUsers}, "User removed !")
+            _ -> (inputLibrary, "User has borrowed one or many books !")
--- Main.hs
+++ Main.hs
@@ -7,14 +7,12 @@ main :: IO ()
 main = do
   putStrLn "Library Management System"
   putStrLn ""
-  putStrLn "Supported Commands: addBook, listBooks, exit"
-
+  putStrLn "Supported Commands: addBook, removeBook, addUser, removeUser, borrow, return, listBooks, listUsers, exit"
   processCommands library
 
 processCommands :: Library -> IO ()
-processCommands library = do
+processCommands currentLibrary = do
   putStr ">"
-
   hFlush stdout
   cmd <- getLine
 
@@ -26,21 +24,80 @@ processCommands library = do
       putStr "Please enter Author: "
       inputAuthor <- getLine
 
-      let newLibrary = addBook inputTitle inputAuthor library
+      let newLibrary = addBook inputTitle inputAuthor currentLibrary
 
       putStrLn "Book added !"
+      processCommands newLibrary -- Looping to get next command
+    "removeBook" -> do
+      putStr "Please enter Book Id: "
+      
+      inputIDStr <- getLine
+      
+      let inputBookId = read inputIDStr :: Int
+          (newLibrary, message) = removeBook inputBookId currentLibrary
+      
+      putStrLn message
+      
+      processCommands newLibrary -- Looping to get next command
+    "addUser" -> do
+      putStr "Please enter Name: "
+      inputName <- getLine
+  
+      let newLibrary = addUser inputName currentLibrary
+  
+      putStrLn "User added !"
+      processCommands newLibrary -- Looping to get next command
+    "removeUser" -> do
+      putStr "Please enter User Id: "
 
+      inputIDStr <- getLine
+
+      let inputUserId = read inputIDStr :: Int
+          (newLibrary, message) = removeUser inputUserId currentLibrary
+      
+      putStrLn message
+      
+      processCommands newLibrary -- Looping to get next command
+    "borrow" -> do
+      putStr "Please enter Book Id: "
+      inputBookIDStr <- getLine
+
+      putStr "Please enter User Id: "
+      inputUserIDStr <- getLine
+      
+      let inputBookId = read inputBookIDStr :: Int
+          inputUserId = read inputUserIDStr :: Int
+          (newLibrary, message) = borrowBook inputBookId inputUserId currentLibrary
+
+      putStrLn message
+      
+      processCommands newLibrary -- Looping to get next command
+    "return" -> do
+      putStr "Please enter Book Id: "
+      inputBookIDStr <- getLine
+  
+      putStr "Please enter User Id: "
+      inputUserIDStr <- getLine
+  
+      let inputBookId = read inputBookIDStr :: Int
+          inputUserId = read inputUserIDStr :: Int
+          (newLibrary, message) = returnBook inputBookId inputUserId currentLibrary
+
+      putStrLn message
+      
       processCommands newLibrary -- Looping to get next command
     "listBooks" -> do
-      putStrLn "Available Books:"
-      availableBooks (books library)
+      availableBooks (books currentLibrary)
+      borrowedBooks  (books currentLibrary)
+      
+      processCommands currentLibrary -- Looping to get next command
+    "listUsers" -> do
+      printUsers (users currentLibrary)
+      
+      processCommands currentLibrary -- Looping to get next command
+    "exit" ->
+      putStrLn "Thank you !"
 
-      putStrLn "Borrowed Books:"
-      borrowedBooks (books library)
-
-      processCommands library -- Looping to get next command
-    "exit" -> putStrLn "Thank you !"
     _ -> do
       putStrLn "Invalid command !"
-      processCommands library
-
+      processCommands currentLibrary -- Looping to get next command
